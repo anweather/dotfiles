2bea90012da6f2677889c22592c2928941667834
Andrew Weatherly <andrew.weatherly@gm.com>
2017-03-30 12:12:25 -0700

    Adding support for gitconfig

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -27,3 +27,29 @@ for dotfile in `ls -1 $link_files`; do
 	test -f ~/.$dotfile && mv ~/.$dotfile ~/.$dotfile.bkup
 	ln -s $link_files/$dotfile ~/.$dotfile
 done
+
+
+declare lock_file=/tmp/lock_
+function locked_exec {
+	hash_function=`which md5 >/dev/null 2>&1 && echo "md5" || echo "base64"`
+	lock_file+=`caller | $hash_function`
+	if mkdir $lock_file 2>/dev/null; then
+		eval $@
+		rmdir $lock_file
+	else
+		echo "Could not get lock ($lock_file). Did not run $1"
+	fi
+}
+
+function setup_git_config {
+	tools_gitconfig="${git_files}/gitconfig"
+	if ! head -n2 ~/.gitconfig | grep "path" | grep "$tools_gitconfig" >/dev/null; then
+		sed -i.bkup "s|path =.*|path = $tools_gitconfig|" ~/.gitconfig
+		rm ~/.gitconfig.bkup
+	fi
+	if ! head -n2 ~/.gitconfig | grep "include\|path" >/dev/null; then
+		echo -e "[include]\n  path = $tools_gitconfig\n$(cat ~/.gitconfig)" > ~/.gitconfig
+	fi
+}
+
+locked_exec setup_git_config
