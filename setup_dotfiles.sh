00a28fb0547dec9374e1efd7ae16229ef78473e4
Andrew Weatherly <anweather@gmail.com>
2017-03-30 12:07:49 -0700

    Adding support for sourcing dotfiles from bash

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
 BASE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-files_dir="$BASE_DIR/files"
 
-for dotfile in `ls -1 $files_dir`; do
+link_files="$BASE_DIR/link_files"
+bash_files="$BASE_DIR/bash_files"
+git_files="$BASE_DIR/git_files"
+
+for dotfile in `ls -1 $bash_files`; do
+	source_str="source $bash_files/$dotfile"
+
+	if [[ -f ~/.$dotfile ]]; then
+		# File exists - make sure we don't already have our source in it
+		result=`grep "$source_str" ~/.$dotfile`
+
+		if [[ $result == "" ]]; then
+			echo "$source_str" >> ~/.$dotfile
+		fi
+	else
+		# File does not exist
+		echo "$source_str" >> ~/.$dotfile
+	fi
+done
+
+for dotfile in `ls -1 $link_files`; do
+	test -L ~/.$dotfile && rm ~/.$dotfile
 	test -f ~/.$dotfile && mv ~/.$dotfile ~/.$dotfile.bkup
-	ln -s $files_dir/$dotfile ~/.$dotfile
+	ln -s $link_files/$dotfile ~/.$dotfile
 done
